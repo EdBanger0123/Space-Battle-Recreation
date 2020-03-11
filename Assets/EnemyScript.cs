43c706d1a221095989dcac3b20eaad689132bb90
EdBanger0123 <EdBanger0123@Gmail.com>
2020-03-11 11:56:42 +0000

    First scene draft

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Transform normandy;
+
+    Rigidbody eRb;
+
+    public float eSpeed, shotTime;
+
+    [SerializeField] GameObject shotGO;
+
+
+    private void Start()
+    {
+        normandy = GameObject.Find("Normandy").transform;
+        eRb = GetComponent<Rigidbody>();
+
+        eSpeed = Random.Range(1f, 1.5f);
+        shotTime = Random.Range(2f, 4f);
+
+        StartCoroutine("Shoot");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(normandy);
+        eRb.velocity = transform.forward * (GameObject.Find("Normandy").GetComponent<ShipScript>().speed - eSpeed);
+        
+    }
+
+    IEnumerator Shoot()
+    {
+        Debug.Log("shooting");
+        Instantiate(shotGO, this.transform.position, this.transform.rotation);
+        
+        yield return new WaitForSeconds(shotTime);
+        StartCoroutine("Shoot");
+
+    }
+}
---
+++ Assets/ShotScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    Rigidbody bRb;
+
+    public Transform normandy; 
+
+    private void Start()
+    {
+        normandy = GameObject.Find("Normandy").transform;
+        bRb = GetComponent<Rigidbody>();
+
+        
+        transform.LookAt(normandy);
+
+        bRb.AddForce(transform.forward * 1000f);
+
+        StartCoroutine("End");
+
+    }
+
+    IEnumerator End()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Destroy(this.gameObject);
+    }
+
+
+}
